f611efe28b9ba257892b7016df8ba63dafa1751e
Hudson Hughes <96hudson@gmail.com>
2018-08-07 19:30:35 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,8 @@ setup(
             'warp_prism._warp_prism',
             ['warp_prism/_warp_prism.c'],
             include_dirs=[np.get_include()],
-            extra_compile_args=['-std=c99', '-Wall', '-Wextra'],
+            extra_compile_args=['-std=c99', '-Wall'],
+            # '-Wextra'
         ),
     ],
     install_requires=[
